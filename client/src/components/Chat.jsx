29291da613b20521acc9d808b346d5e3982dd822
ROHAN BHATIA <rohanb459@gmail.com>
2023-08-18 16:07:01 +0530

    files and attachments upload

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -50,25 +50,35 @@ const Chat = () => {
     }
   }
 
-  function sendMessage(e) {
-    e.preventDefault();
-    console.log("sending");
+  function sendMessage(e, file=null) {
+    if(e)e.preventDefault();
+
     ws.send(
       JSON.stringify({
         recipient: selectedUserId,
         text: newMessageText,
+        file
       })
     );
-    setNewMessageText("");
-    setMessages((prev) => [
-      ...prev,
-      {
-        text: newMessageText,
-        sender: id,
-        recipient: selectedUserId,
-        _id: Date.now(),
-      },
-    ]);
+    
+    if(file)
+    {
+      axios.get('/messages/' + selectedUserId).then(res=>{
+        setMessages(res.data);
+      })
+    }
+    else{
+      setNewMessageText("");
+      setMessages((prev) => [
+        ...prev,
+        {
+          text: newMessageText,
+          sender: id,
+          recipient: selectedUserId,
+          _id: Date.now(),
+        },
+      ]);
+    }
   }
 
   function logOut(){
@@ -107,6 +117,18 @@ const Chat = () => {
       });
     }
   }, [selectedUserId]);
+
+  function sendFile(ev)
+  {
+    const reader = new FileReader();
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onload = () =>{
+      sendMessage(null, {
+        name: ev.target.files[0].name,
+        data: reader.result
+      })
+    }
+  }
   const onlinePeopleExclOurUser = { ...onlinePeople };
   delete onlinePeopleExclOurUser[id];
 
@@ -176,6 +198,17 @@ const Chat = () => {
                       }
                     >
                       {message.text}
+                      {message.file && (
+                        <div className="">
+                          <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+  <path fill-rule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clip-rule="evenodd" />
+</svg>
+                            {message.file}
+                          </a>
+
+                        </div>
+                      )}
                     </div>
                   </div>
                 ))}
@@ -194,6 +227,13 @@ const Chat = () => {
               placeholder="Type your message here....."
               className="bg-white flex-grow border p-2 rounded-md"
             />
+            <label className=" cursor-pointer bg-blue-400 rounded-md p-2 text-gray-800 border border-blue-500">
+            <input type="file" className="hidden" onChange={sendFile} />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clip-rule="evenodd" />
+</svg>
+
+            </label>
             <button
               type="submit"
               className="bg-blue-500 p-2 text-white rounded-md"
